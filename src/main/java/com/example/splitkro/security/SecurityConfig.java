d17c92b4284e1cee48401c32088baf8c89e69ded
Shreya211149 <shreyamidya2122002@gmail.com>
2026-04-02 17:34:28 +0530

    make /api/users public for member lookup

--- src/main/java/com/example/splitkro/security/SecurityConfig.java
+++ src/main/java/com/example/splitkro/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/users/register",
                                 "/api/users/login",
+                                "/api/users",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/api-docs/**"
